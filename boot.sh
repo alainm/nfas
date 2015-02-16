19553b0e4516b5ad648d61b538154d31e1b88e5d
Alain Mouette <alainm@bonseletrons.com.br>
2015-02-16 09:58:46 -0200

    Configura ACPI para desligamento do VirtualBox

--- boot.sh
+++ boot.sh
@@ -2,5 +2,13 @@
 
 # Este é o script que dá boot na instalação.
 # precisa ser feito download e executado manualmente
+# penas o mais básico é feito aqui
 
 echo "Executando boot.sh, parabéns..."
+
+# Habilita ACPI para fechamento da VM do VitrualBox
+# site: http://virtbjorn.blogspot.com.br/2012/12/how-to-make-your-vm-respond-to-acpi.html?m=1
+# TODO: bom para VirtualBox, verificar viabilidade para VPS
+yum -y install acpid
+chkconfig acpid on
+service acpid start
