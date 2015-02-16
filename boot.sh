dd3ca6e92423f928e2316087e15e2207cccd386b
Alain Mouette <alainm@bonseletrons.com.br>
2015-02-16 09:59:19 -0200

    Configura Rede Sempre ligada

--- boot.sh
+++ boot.sh
@@ -6,6 +6,9 @@
 
 echo "Executando boot.sh, parabéns..."
 
+# Configura a ETH0 para default sempre ligada
+sed '/ONBOOT/s/no/yes/g' -i /etc/sysconfig/network-scripts/ifcfg-eth0
+
 # Habilita ACPI para fechamento da VM do VitrualBox
 # site: http://virtbjorn.blogspot.com.br/2012/12/how-to-make-your-vm-respond-to-acpi.html?m=1
 # TODO: bom para VirtualBox, verificar viabilidade para VPS
