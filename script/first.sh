9af6ffba3e5d2ea62dbc30f0644e2e13d8fe0db9
Alain <alain@alain3.cosmodata.inet>
2015-02-18 17:57:32 -0200

    first.sh: altera rc.local

--- script/first.sh
+++ script/first.sh
@@ -7,5 +7,5 @@ echo "Parabéns, você está rotando o /script/first.sh"
 # Altera o /etc/rc.d/rc.local para chamar o /script/autostart.sh
 cat /etc/rc.d/rc.local | grep "autostart.sh"
 if [ $? -ne 0 ]; then
-  echo -e "\n# NFAS: executa scripts de inicialização\n/script/autostart.sh\n"
+  echo -e "\n# NFAS: executa scripts de inicialização\n/script/autostart.sh\n" >> /etc/rc.d/rc.local
 fi
