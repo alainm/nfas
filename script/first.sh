eca49a4cea63abb6d19444cc6e00c5a3c4831deb
Alain <alain@alain3.cosmodata.inet>
2015-02-18 18:42:17 -0200

    first.sh: teste com VM virgem parou no yum

--- script/first.sh
+++ script/first.sh
@@ -20,7 +20,7 @@ fi
 # Determina se está rodando em um VirtualBox
 # site: http://stackoverflow.com/questions/12874288/how-to-detect-if-the-script-is-running-on-a-virtual-machine
 # A variável fica guardada no diretório de dados, para usar deve ser incluida com o camando ". "
-yum install dmidecode
+yum -y install dmidecode
 dmidecode  | grep -i product | grep VirtualBox
 if [ $? -eq 0 ] ;then
   IS_VIRTUALBOX="Y"
