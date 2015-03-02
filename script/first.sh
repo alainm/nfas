d25960def96fb616e545f51c0116c6230b3ef97c
alainm <alain@alain3.cosmodata.inet>
2015-03-01 21:43:05 -0300

    VirtualBox: movido teste para dentro do script virtualbox.sh

--- script/first.sh
+++ script/first.sh
@@ -36,17 +36,7 @@ if [ $? -ne 0 ]; then
   echo -e "\n# NFAS: executa scripts de inicialização\n/script/autostart.sh\n" >> /etc/rc.d/rc.local
 fi
 
-# Determina se está rodando em um VirtualBox
-# site: http://stackoverflow.com/questions/12874288/how-to-detect-if-the-script-is-running-on-a-virtual-machine
-# A variável fica guardada no diretório de dados, para usar deve ser incluida com o comando ". "
-yum -y install dmidecode
-dmidecode  | grep -i product | grep VirtualBox
-if [ $? -eq 0 ] ;then
-  IS_VIRTUALBOX="Y"
-else
-  IS_VIRTUALBOX="N"
-fi
-echo "IS_VIRTUALBOX=$IS_VIRTUALBOX" > /script/info/virtualbox
+# Roda as configuraçãoes próprias para o VirtualBox
 /script/virtualbox.sh
 
 # ===== FIM do first.sh =====
--- script/virtualbox.sh
+++ script/virtualbox.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
+set -x
 
 # Script para ajustes necessários apenas ao VirtualBox
 # é chamado pelo /script/first.sh
 
 echo "Rodando o Script de inicialização: /script/virtualbox.sh"
+
+# Determina se está rodando em um VirtualBox
+# site: http://stackoverflow.com/questions/12874288/how-to-detect-if-the-script-is-running-on-a-virtual-machine
+# A variável fica guardada no diretório de dados, para usar deve ser incluida com o comando ". "
+yum -y install dmidecode
+dmidecode  | grep -i product | grep VirtualBox
+if [ $? -eq 0 ] ;then
+  IS_VIRTUALBOX="Y"
+else
+  IS_VIRTUALBOX="N"
+fi
+echo "IS_VIRTUALBOX=$IS_VIRTUALBOX" > /script/info/virtualbox.var
+
+# Se não é VirtualBox, retorna sem erro
+if [ "$IS_VIRTUALBOX" == "N" ]; then
+  exit 0
+fi
 
 # VirtualBox: configura a ETH0 para default sempre ligada
 sed '/ONBOOT/s/no/yes/g' -i /etc/sysconfig/network-scripts/ifcfg-eth0
