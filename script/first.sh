72da288dccef38d2b1f5b95cadbd8c4e8a12f9e6
alainm <alain@alain3.cosmodata.inet>
2015-04-05 20:26:36 -0300

    first.sh: incluidos programas telnet e hosts

--- script/first.sh
+++ script/first.sh
@@ -27,7 +27,7 @@ rpm -e postfix
 # Repositório auxiliar: https://fedoraproject.org/wiki/EPEL
 yum -y install epel-release
 # Instalar pacotes extra
-yum -y install man nano mcedit
+yum -y install man nano mcedit telnet bind-utils
 
 # Altera o /etc/rc.d/rc.local para chamar o /script/autostart.sh
 cat /etc/rc.d/rc.local | grep "autostart.sh"
