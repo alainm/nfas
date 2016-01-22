5a96bf4017522b0884bd67dc507e4397b867e6ac
alainm <alainm@bonseletrons.com.br>
2016-01-22 21:45:25 -0200

    progs.sh: download de node.js com opções LTS e Stable

--- script/progs.sh
+++ script/progs.sh
@@ -40,14 +40,48 @@ function SetupGit(){
 }
 
 #-----------------------------------------------------------------------
+# Fornece a versão do Node Stable mais novo
+function GetVerNodeStable(){
+  # usa o WGET com "--no-dns-cache -4" para melhorar a velocidade de conexão
+  local NEW_NODE=$(wget --quiet --no-dns-cache -4 http://nodejs.org/dist/latest/ -O - | sed -n 's/.*\(node.*linux-x64\.tar\.gz\).*/\1/p' | sed -n 's/node-\(v[0-9\.]*\).*/\1/p')
+  echo "$NEW_NODE"
+}
+
+#-----------------------------------------------------------------------
+# Fornece a versão do Node LTS mais novo
+# procura na tabela em https://nodejs.org/dist/index.tab
+#   foi dica daqui: https://github.com/nodejs/node/issues/4569#issuecomment-169746908
+function GetVerNodeLts(){
+  # Baixa lista oficial de versões, coluna1: versão, coluna10: novme lts,
+  #   tira primeira linha header, só linhas co nome Lts, sort, última linha, versão
+  local LTS_NODE=$(wget --quiet --no-dns-cache -4 https://nodejs.org/dist/index.tab -O - | awk '{print $1 "\t" $10}' | tail -n +2 | \
+    awk '$2 != "-"' | sort | tail -n 1 | cut -f1)
+  echo "$LTS_NODE"
+}
+
+#-----------------------------------------------------------------------
+# Fornece a versão atual do Node instalada
+function GetVerNodeAtual(){
+  if which node >/dev/null; then
+    echo $(node -v)
+  else
+    echo "não instalado"
+  fi
+}
+
+#-----------------------------------------------------------------------
 # Rotina para instalar Node.js (latest)
+# site: https://nodejs.org/dist/v4.2.6/node-v4.2.6-linux-x64.tar.gz
+# Uso: NodeInstall <versão>
 function NodeInstall(){
-  local NEW_NODE=$(curl --silent --location http://nodejs.org/dist/latest/ | sed -n 's/.*\(node.*linux-x64\.tar\.gz\).*/\1/p')
-  echo "Instalar Node: $NEW_NODE"
+  # Localização e nome do arquivo da versão solicitada
+  local NODE_URL="https://nodejs.org/dist/$1/node-$1-linux-x64.tar.gz"
+  local NODE_FILE="node-$1-linux-x64.tar.gz"
+  echo "Instalar Node: $NODE_FILE"
   # baixa no diretório root
-  wget -r http://nodejs.org/dist/latest/$NEW_NODE -O /root/$NEW_NODE
+  wget --no-dns-cache -4 -r $NODE_URL -O /root/$NODE_FILE
   pushd /usr/local
-  tar --strip-components 1 -xzf /root/$NEW_NODE
+  tar --strip-components 1 -xzf /root/$NODE_FILE
   popd
   # Mostra versões para debug
   node -v
@@ -58,19 +92,25 @@ function NodeInstall(){
 # Instala programas pré configurados
 function ProgsInstall(){
   # Última versão do Node.js:
-  LAST_NODE="$(curl --silent --location http://nodejs.org/dist/latest/ | sed -n 's/.*\(node.*linux-x64\.tar\.gz\).*/\1/p' | sed -n 's/node-\(v[0-9\.]*\).*/\1/p')"
-  NODE_MSG=" versão $LAST_NODE (latest)"
-  OPTIONS=$(whiptail --title "$TITLE"                                   \
-    --checklist "\nSelecione os programas que deseja Instalar:" 22 75 1 \
-    'Node.js' "$NODE_MSG" YES   \
+  local LTS_NODE=$(GetVerNodeLts)
+  local STB_NODE=$(GetVerNodeStable)
+  local NODE1_MSG="versão LTS          : $LTS_NODE. atual=$(GetVerNodeAtual)"
+  local NODE2_MSG="versão latest/stable: $STB_NODE"
+  local OPTIONS=$(whiptail --title "$TITLE"                                   \
+    --checklist "\nSelecione os programas que deseja Instalar:" 22 75 2 \
+    'Node-LTS'    "$NODE1_MSG" YES   \
+    'Node-Stable' "$NODE2_MSG" NO    \
     3>&1 1>&2 2>&3)
   if [ $? == 0 ]; then
     #--- Instala programas selecionados
     for OPT in $(echo $OPTIONS | tr -d '\"'); do
       echo "Instalação: $OPT"
       case $OPT in
-        "Node.js")
-          NodeInstall
+        "Node-LTS")
+          NodeInstall $LTS_NODE
+        ;;
+        "Node-Stable")
+          NodeInstall $STB_NODE
         ;;
       esac
     done
