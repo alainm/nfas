dc4c6f88e8352b2f25725b72c48c41a038a539a9
alainm <alainm@bonseletrons.com.br>
2016-03-13 21:24:55 -0300

    console.sh: mensagem de segurança no JuiceSSH

--- script/console.sh
+++ script/console.sh
@@ -68,11 +68,11 @@ function AddColorToFile(){
       echo 'export PS1="\[$(tput bold)\]\[$(tput setaf 2)\][\[$(tput setaf 4)\]\u\[$(tput setaf 2)\]@\[$(tput setaf 4)\]\h \[$(tput setaf 2)\]\W\[$(tput setaf 2)\]]\[$(tput setaf 2)\]\\$ \[$(tput setaf 7)\]\[$(tput sgr0)\]"' >> $ARQ
       echo '# Mensagem de segurança e boas vindas, precisa do sudo para acesso ao Log'    >> $ARQ
       echo 'sudo -E /script/console.sh --wellcome'                                        >> $ARQ
-      echo 'if [ "$TERM" != "linux" ] && [ -n "$SSH_TTY" ]; then'                         >> $ARQ
+      echo 'if [ -n "$SSH_TTY" ]; then'                                                   >> $ARQ
       echo '  # Só executa se login pelo SSH'                                             >> $ARQ
       echo '  /script/console.sh --wellcome'                                              >> $ARQ
       echo 'else'                                                                         >> $ARQ
-      echo '  echo -e "\n Acesso seguro via CONSOLE! Bemvindo ao \"$(hostname)\"\n"'      >> $ARQ
+      echo '  echo -e "\n Acesso via CONSOLE! Bemvindo ao \"$(hostname)\"\n"'      >> $ARQ
       echo 'fi'                                                                           >> $ARQ
     fi
     echo ''                                                                               >> $ARQ
